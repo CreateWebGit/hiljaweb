8c2fcfb8faaed654c96b71af4a88ecd7890d6a03
emilavara <emilavara@hotmail.com>
2025-07-04 12:54:04 +0200

    done with temp site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import '$lib/css/main.scss'
+</script>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import IconAnimation from "$lib/components/IconAnimation.svelte";
+</script>
+
+<svelte:head>
+    <title>Hilja Logistics – Sidan är under konstruktion</title>
+    <meta name="description" content="Hilja Logistics erbjuder kompletta logistiklösningar – från 3PL-lager till frakt. Just nu bygger vi vår nya hemsida." />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="UTF-8" />
+
+    <meta property="og:title" content="Hilja Logistics – Sidan är under konstruktion" />
+    <meta property="og:description" content="Vi bygger en ny digital terminal! Under tiden kan du följa oss på LinkedIn." />
+    <meta property="og:url" content="https://hiljalogistics.se" />
+    <meta property="og:type" content="website" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Hilja Logistics – Sidan är under konstruktion" />
+    <meta name="twitter:description" content="Vi bygger en ny digital terminal! Under tiden kan du följa oss på LinkedIn." />
+</svelte:head>
+
+<div class="stripes">
+    <div class="stripe"></div>
+    <div class="stripe blue"></div>
+</div>
+
+<main>
+    <div class="logo-container">
+        <img src="/images/hilja-logo.svg" alt="hilja logo" width="247" height="95">
+    </div>
+    <div class="text-container text-center">
+        <h1 class="mb-1">Sidan lastas just nu!</h1>
+        <h2>Vi är mitt i en logistisk operation av högsta klass. <br class="hide-mobile"/> Hemsidan är på väg in i lagret!</h2>
+        <p class="mt-2">Vi erbjuder smidiga logistiklösningar för företag i hela Sverige. <br class="hide-mobile"/> Vi hanterar allt från 3PL-lager till transport och frakt. Alltid med gratis konsultation.</p>
+    </div>
+    <div class="footer d-flex gap-1">
+        <a class="button" href="https://www.linkedin.com/company/hilja-logistics" target="_blank">Följa oss på LinkedIn</a>
+        <a class="button stroked" href="mailto:hilja@hiljalogistics.se" target="_blank">Mejla oss</a>
+    </div>
+</main>
+
+<!-- <IconAnimation/> -->
